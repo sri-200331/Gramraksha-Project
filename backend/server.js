accba95ebfe862d4508e0cf25df8b44a58bb2837
sri-200331 <poomanibharathi07@gmail.com>
2026-03-04 13:11:28 +0530

    updates

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // ─── Middleware ──────────────────────────────────────────────────────────────
 app.use(cors({
-    origin: [FRONT_END_URL],
+    origin: [process.env.FRONT_END_URL],
     credentials: true,
 }));
 app.use(express.json());
